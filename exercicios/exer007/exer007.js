968ccca1d26e62a0ed656f7d76f789034258fb11
Juan Holanda <123557653+JuanHolandaMarques@users.noreply.github.com>
2023-02-22 20:49:50 -0300

    condições
    mais um dia quebrando a cabeça com js

---
+++ exercicios/exer007/exer007.js
@@ -0,0 +1,13 @@
+var veloc = 100
+var quantAviso = 0
+
+if ( veloc >= 100 ){
+    console.log('VAI DEVAGAR RAPAZ!')
+    quantAviso += 1;
+}else{
+    console.log('isso, devagarzinho que os homi tão ai')
+}
+
+if (quantAviso >= 1){
+    console.log('maacho, eu já falei')
+}
