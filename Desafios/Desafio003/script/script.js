aa2987539d802618e576c9e7199a6999ca17cf4a
Juan Holanda <123557653+JuanHolandaMarques@users.noreply.github.com>
2023-03-01 17:23:04 -0300

    desafio003 terminado

--- Desafios/Desafio003/script/script.js
+++ Desafios/Desafio003/script/script.js
@@ -1,9 +1,42 @@
 function contar(){
-    var inicio = document.getElementById('inicio').value
-    var fim = document.getElementById('fim').value
-    var passo = document.getElementById('passo').value
+    var inicioTxt = document.getElementById('inicio')
+    var fimTxt = document.getElementById('fim')
+    var passoTxt = document.getElementById('passo')
     var resultado = document.getElementById('result') 
-    var sectionResutado = document.getElementById('sectionResultado')
+
+    if (inicioTxt.value.length == 0 || fimTxt.value.length == 0 || passoTxt.value.length == 0 ){
+
+        resultado.innerHTML = '[ERRO] Dados incompletos'
+
+    }else if( passoTxt.value == 0){
+
+        resultado.innerHTML = '[ERRO] <strong>passo</strong> inválido'
+
+    }else{
+
+        var inicio = Number(inicioTxt.value)
+        var fim = Number(fimTxt.value)
+        var passo = Number(passoTxt.value)
+        var contador = inicio
+
+        if(inicio <= fim){
+            for (contador = inicio; contador <= fim; contador += passo){
+                resultado.innerHTML += ` ${contador} \u{1F449} `
+            }
+            
+        }else{
+           for (contador = inicio; contador >= fim; contador -= passo ){
+                resultado.innerHTML += ` ${contador} \u{1F449} `
+           } 
+        }
+        resultado.innerHTML += ` \u{1F3C1}`
+
+    }
+
+
+
+   
+
 
 
 }
